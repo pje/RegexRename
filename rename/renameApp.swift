a1fd73d855417abcb96fb9419995ce2bb7cca7b5
Patrick Ellis <patrick.j.ellis@gmail.com>
2021-08-22 13:40:34 -0400

    Initial Commit

---
+++ rename/renameApp.swift
@@ -0,0 +1,17 @@
+//
+//  renameApp.swift
+//  rename
+//
+//  Created by Patrick Ellis on 8/22/21.
+//
+
+import SwiftUI
+
+@main
+struct renameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
